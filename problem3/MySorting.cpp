7283b25b19fdf2f9298adea4c004ef8c99b55234
Matthew Aragaw <maragaw24@gmail.com>
2020-12-04 23:45:28 -0800

    problem3 bug

--- problem3/MySorting.cpp
+++ problem3/MySorting.cpp
@@ -27,19 +27,21 @@ string customSort(string a[], size_t size){
         transform(upper.begin(), upper.end(), upper.begin(), ::toupper); //transform string to uppercase
         a[i] = upper; //change element in array to that new uppercase string
     }
+    
     //sort them from shortest to longest
     //use alphabetical order to break ties
     //assume its only alphabetical letters
     for(size_t i = 1; i < size; i++){
         item = a[i];
         shiftIndex = i-1;
-        while (shiftIndex >=0 && (a[shiftIndex].length() > item.length())){
+        //while loop 1
+        while (shiftIndex >=0 && a[shiftIndex].length() > item.length()){
             a[shiftIndex+1] = a[shiftIndex];
             shiftIndex -=1;
         }
         //breaks when shiftindex not bigger than 0, string at shiftindex comes first alphabetically
-        if(a[shiftIndex].length() == item.length()){
-            while(shiftIndex >= 0 && a[shiftIndex]>item){ //memory leak
+        if(a[shiftIndex].length() == item.length()){ //runs when they are the same length to break the tie
+            while(shiftIndex >= 0 && a[shiftIndex].compare(item)>0 && a[shiftIndex].length()==item.length()){ 
                 a[shiftIndex+1] = a[shiftIndex];
                 shiftIndex -=1;
             }
--- problem3/testSorting.cpp
+++ problem3/testSorting.cpp
@@ -6,6 +6,7 @@ int main() {
 
 
   // Test case #1
+  
   string a[] {"If", "music", "be", "the", "food", "of", "love", "play", "on"};
   size_t a_size = 9;
   string expected_1 = "BE,IF,OF,ON,THE,FOOD,LOVE,PLAY,MUSIC";
@@ -28,5 +29,12 @@ int main() {
   size_t d_size = 4;
   string expected_4 = "A,B,C,D";
   ASSERT_EQUALS(expected_4,customSort(d, d_size));
+
+  string e[] {"monuments", "powerful", "in", "of", "contents", "princes", "but", "nor", "outlive", "marble", "not", "the", "gilded", "bright", "you", "more", "these", "shine", "this", "rhyme", "shall","shall"};
+  size_t e_size = 22;
+  string expected_5 = "IN,OF,BUT,NOR,NOT,THE,YOU,MORE,THIS,RHYME,SHALL,SHALL,SHINE,THESE,BRIGHT,GILDED,MARBLE,OUTLIVE,PRINCES,CONTENTS,POWERFUL,MONUMENTS";
+  ASSERT_EQUALS(expected_5, customSort(e, e_size));
+ // string actual = "BRIGHT,BUT,CONTENTS,IN,OF,GILDED,MORE,NOR,NOT,RHYME,THE,YOU,THIS,SHALL,SHALL,SHINE,THESE,MARBLE,OUTLIVE,PRINCES,POWERFUL,MONUMENTS";
+ // customSort(e, e_size);
   return 0;
 }
