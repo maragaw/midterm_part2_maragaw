4a2628c0da19c4116bc3b0163522b4c6bdf9323c
Matthew Aragaw <maragaw24@gmail.com>
2020-12-05 01:00:04 -0800

    Update MyHashTable.cpp

--- problem2/MyHashTable.cpp
+++ problem2/MyHashTable.cpp
@@ -127,35 +127,3 @@ MyHashTable::~MyHashTable() {
 	
 	
 }
-
-int main() {
-
-  // A sample test.
-  // A sample test.
-  MyHashTable ht;
-  ht.insertItem("Peter Parker", 27437);
-  ht.insertItem("Tony Stark", 49872);
-  ht.insertItem("Bruce Banner", 39857);
-  ht.insertItem("Steven Rogers", 12984);
-  string expected = "0:[(Tony Stark,49872)]\n1:[]\n2:[(Peter Parker,27437)(Bruce Banner,39857)(Steven Rogers,12984)]\n3:[]\n4:[]\n";
- // ASSERT_EQUALS(expected, ht.toString());
-  
-  // Write your own tests below.
-    
-    //cout<<ht.toString()<<endl;
-    
-    cout<<ht.toString()<<endl;
-    MyHashTable h2;
-    h2 = ht;
-    cout<<h2.toString()<<endl;
-    ht.deleteItem("Tony Stark");
-    cout<<ht.toString()<<endl;
-    cout<<h2.toString()<<endl; //copy constructor/assignment operator working
-
-    
-
-   
-
-
-  return 0;
-}
